469457c6563abce66b087551060e9a310dbac854
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-09-21 21:42:06 -0400

    created home page component with basic styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
+import React, {Component} from 'react'
+import Home from './components/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          App component here
-        </p>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    render() {
+        return (
+          <div className="App">
+            <header className="App-header">
+              <p>
+                <Home /> 
+              </p>
+            </header>
+          </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+
+
+    render() {
+        return (
+            <div className='flex flex-col justify-center items-center bg-black w-full h-screen'>
+                <h1 className='text-white text-5xl'>Home</h1>
+                <div className='grid w-3/4 h-[500px] bg-white'>
+                    {/* place tile components here */}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home
