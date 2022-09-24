8607a18d448b5a81cb6063ab96c1d8290ddd7668
ecampos <ecampos.jr54@gmail.com>
2022-09-24 15:32:18 -0500

    add navbar and react router and linked pages with react router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,30 @@ import './App.css';
 import React, {Component} from 'react'
 import Home from './components/Home'
 import NewExercise from './components/NewExercise'
+import NavBar from './components/NavBar'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 class App extends Component {
     
 
     render() {
         return (
-          <>
-            <Home /> 
-            <NewExercise />
-          </>
+          // <>
+          //   <Home /> 
+          //   <NewExercise />
+          // </>
+          
+          <Router>
+            <NavBar />
+            <Routes>
+              <Route path='/'element={<Home />}/>
+            </Routes>
+          </Router>
         );
     }
 }
