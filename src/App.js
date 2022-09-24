570d50fbf3cf6feee35ee65bbfe40f663c306285
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-09-24 10:37:53 -0400

    created this.state for searchURL for api

--- src/App.js
+++ src/App.js
@@ -4,16 +4,34 @@ import Home from './components/Home'
 import NewExercise from './components/NewExercise'
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            baseURL: 'https://wger.de/api/v2/',
+            apiKey: `api_key=${process.env.REACT_APP_API_KEY}`,
+            // need /exercise, /exerciseimage, /muscle
+            category: [
+                'exercise/',
+                'exerciseimage/',
+                'muscle/'
+            ],
+            jsonFormat: '?format=json/',
+            limitQuery: '&limit=',
+            // display default number of exercises
+            limit: 5,
+            languageQuery: '&language=2',
+            searchURL: ''
+        }
+    }
+
     render() {
+        console.log(this.state)
         return (
-          <div className="App">
-            <header className="App-header">
-              <p>
-                <Home /> 
-                <NewExercise />
-              </p>
-            </header>
-          </div>
+          <>
+            <Home /> 
+            <NewExercise />
+          </>
         );
     }
 }
