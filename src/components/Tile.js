24c257cc4d58d1c43832ab7cc51e43b90a74cc41
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-09-24 16:16:31 -0400

    able to render muscle group onto page now

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react'
 
 class Tile extends Component {
     render(){
-        // let muscle = this.props.categoryArray.find(category => category.id === this.props.category)
-        // console.log(muscle)
+        let muscle = ''
         return (
             <div className="tile-container">
-                {this.props.exerciseName}
-                {this.props.categoryArray.forEach(
-                    category => {
-                        if(category.id == this.props.category) {
-                            console.log(category.name)
-                        }
-                    } 
-                )}
-                {this.props.category}
-                {/* {muscle} */}
-                {/* {this.props.categoryArray[0]} */}
+                <p>{this.props.exerciseName}</p>
+                {
+                    this.props.categoryArray.forEach(
+                        category => {
+                            if(category.id == this.props.category) {
+                                console.log(category.name)
+                                muscle = category.name
+                            }
+                        } 
+                    )
+                }
+                <p>Muscle Group: {muscle}</p>
             </div>
         )
     }
