2943c589982df30dd154bdcd216750fe540766e1
ecampos <ecampos.jr54@gmail.com>
2022-09-27 20:16:36 -0500

    import react to ShowAPIExercise, add ShowAPIExercise function component and export default same

---
+++ src/components/ShowAPIExercise.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+const ShowAPIExercise = () => {
+    return(
+        <div>
+            <Link to ='/ShowAPIExercise'>
+                <p>Show Time 🏀</p>
+            </Link>
+        </div>
+    )
+}
+
+export default ShowAPIExercise
