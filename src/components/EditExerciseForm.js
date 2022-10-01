c6af622f17a58695c04d4fb2349f65c846c30708
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-10-01 14:44:02 -0400

    added other form data

--- src/components/EditExerciseForm.js
+++ src/components/EditExerciseForm.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 
 
 class EditExerciseForm extends Component {
-    
+
     render () {
         return (
-            <form className='
+            <form onSubmit={this.props.handleEditExercise} className='
                 flex 
                 flex-col 
                 w-4/5 
@@ -15,16 +15,53 @@ class EditExerciseForm extends Component {
                 rounded 
                 mx-8
             '>
-
-                {/* NAME INPUT FIELD */}
                 <input
                     id='name'
                     type='text'
-                    // value={this.state.name} need to change this to the current value. passed down as a prop?
+                    // value={this.props.exercises} // need to change this to the current value. passed down as a prop?
                     onChange={this.handleChange}
                     placeholder='Exercise Name'
                     className='border rounded p-1.5 my-1'>
                 </input>
+                <input
+                        id='description'
+                        type='text'
+                        // value={this.state.description}
+                        onChange={this.handleChange}
+                        placeholder='Description'
+                        className='border rounded p-1.5 my-1'>
+                    </input> 
+                    <input
+                        id='exerciseImage'
+                        type='text'
+                        // value={this.state.exerciseImage}
+                        onChange={this.handleChange}
+                        placeholder='Image URL'
+                        className='border rounded p-1.5 my-1'>
+                    </input>
+                    <input
+                        id='muscles'
+                        type='text'
+                        // value={this.state.muscles}
+                        onChange={this.handleChange}
+                        placeholder='Muscle Group'
+                        className='border rounded p-1.5 my-1'>
+                    </input>
+                    <input
+                        id='notes'
+                        type='text'
+                        // value={this.state.notes}
+                        onChange={this.handleChange}
+                        placeholder='Notes'
+                        className='border rounded p-1.5 my-1'>
+                    </input>
+
+                    {/* submit button */}
+                    <input 
+                        type='submit'
+                        value='Update Exercise'
+                        className='bg-[#ABC8CA] p-2 m-2 rounded cursor-pointer'
+                    />
             </form>
         )
     }
