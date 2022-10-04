aa6977dd74fba4babf9bfa13bab411d6c111c4f8
ecampos <ecampos.jr54@gmail.com>
2022-10-04 00:23:07 -0500

    add show container custom class component, import react and export same component

---
+++ src/components/ShowContainerCustom.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ShowContainerCustom extends Component {
+    render(){
+        return(
+            <div>
+                test
+            </div>
+        )
+    }
+}
+
+export default ShowContainerCustom
