4b3a47771cf520bcd0194e03ad4b155aa95bf091
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-10-04 22:09:12 -0400

    able to render each item on show page dynamically

--- src/components/CustomExerciseTile.js
+++ src/components/CustomExerciseTile.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 class CustomExerciseTile extends Component {
     render(){
+        
         return (
             <>
             <div id="tile" className='border-black border-2 rounded-md bg-white p-2 lg:h-44 md:h-36 sm:h-32 h-32 lg:w-44 md:w-36 sm:w-32 w-32'>
--- src/components/ShowCustomExercise.js
+++ src/components/ShowCustomExercise.js
@@ -3,22 +3,28 @@ import ShowContainerCustom from './ShowContainerCustom';
 
 class ShowCustomExercise extends Component {
     render(){
+
+        const customExerciseId = window.location.search.slice(4);
+
         return(
             <div>
-            {this.props.customExercises.map((exercise, index) => {
-                // console.log(exercise.category, 'line 9 of showcustomexercise')
-                // console.log(this.props.customExercises, 'line 10 of customExercises')
-                return(
-                    <ShowContainerCustom
-                    key={index}
-                    exerciseName={exercise.name}
-                    exerciseDescription = {exercise.description}
-                    category = {exercise.muscles}
-                    categoryArray = {this.props.categories}
-                    />)}
-                    
-                )}
-        </div>
+                {this.props.customExercises.map((exercise, index) => {
+                    // console.log(exercise.category, 'line 9 of showcustomexercise')
+                    // console.log(this.props.customExercises, 'line 10 of customExercises')
+                    if(exercise._id === customExerciseId) {
+                        return(
+                            <ShowContainerCustom
+                            key={index}
+                            id={customExerciseId}
+                            exerciseName={exercise.name}
+                            exerciseDescription = {exercise.description}
+                            category = {exercise.muscles}
+                            categoryArray = {this.props.categories}
+                            />)
+                    }
+                }        
+            )}
+            </div>
         )
     }
 }
