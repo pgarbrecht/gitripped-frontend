17db57b94e556170f8df02e5fd3da9aef6c8e7ca
ecampos <ecampos.jr54@gmail.com>
2022-10-06 21:41:59 -0500

    added pseudocode for regex in showcontainerapi

--- src/components/ShowContainerAPI.js
+++ src/components/ShowContainerAPI.js
@@ -19,10 +19,20 @@ class ShowContainerAPI extends Component {
             // HTML tag with a null string.
             
             return str.replace( /(<([^>]+)>)/ig, '');
+                //Our analysis:
+                    //we're using the above regular expression to replace our HTML tags with nothing using an empty string.
+                        //Source for understanding: https://stackoverflow.com/questions/9444876/what-is-replace-ig-doing
+                    //the parentheses surround what we're looking for with the replace method:
+                        //< = replaces the left tags with nothing.
+                        // [^>]=replaces the tag letter (p, h1, etc.) immediately after the opening left tag
+                        // + = the '+' symbol is a repetition operator which executes the method multiple times
+                            //"repetition operator" source: https://stackoverflow.com/questions/3850217/what-is-the-meaning-of-in-a-regex
+                        // > = replaces the right tag with nothing
+                        // /ig = 'i' makes the regular expression case insensitive and 'g' means that all occurrences of tags that the method finds on the rendered page should be replaced.
         }
-        // SOURCE: https://www.geeksforgeeks.org/how-to-strip-out-html-tags-from-a-string-using-javascript/#:~:text=To%20strip%20out%20all%20the,innerText%20property%20from%20HTML%20DOM
+        // SOURCE for stripping HTML tags with regex: https://www.geeksforgeeks.org/how-to-strip-out-html-tags-from-a-string-using-javascript/#:~:text=To%20strip%20out%20all%20the,innerText%20property%20from%20HTML%20DOM
             //We used this method to remove html tags from strings and adapted it to our own needs.
-
+            
         let description = removeTags(this.props.exerciseDescription)
         
         return (
